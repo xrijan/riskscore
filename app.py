ed228a2b3a46067aefd6483b6db8c752d2800ea9
Rijan Rayamajhi <rayarijan5051@gmail.com>
2024-09-16 20:14:00 +0530

    first model test release

---
+++ app.py
@@ -0,0 +1,50 @@
+# app.py
+from flask import Flask, request, jsonify, render_template
+import pickle
+import gzip
+import pandas as pd
+
+app = Flask(__name__)
+
+# Decompress and load the saved scaler
+with gzip.open('standard_scaler.pkl.gz', 'rb') as scaler_file_in:
+    scaler = pickle.load(scaler_file_in)
+
+# Decompress and load the saved random forest model
+with gzip.open('random_forest_risk_score_model_compressed.pkl.gz', 'rb') as model_file_in:
+    model = pickle.load(model_file_in)
+
+
+# Define the features used for prediction
+features = ['cost_t', 'age', 'dem_female', 'race', 'biomarkers', 'comorbidity']
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# API route to predict risk score
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Parse input data from the request
+        data = request.get_json()
+        
+        # Convert data to DataFrame
+        df = pd.DataFrame([data])
+        
+        # Check if all required features are present
+        if all(feature in df.columns for feature in features):
+            X = df[features]
+            X_scaled = scaler.transform(X)
+            predicted_risk_score = model.predict(X_scaled)[0]
+            
+            # Return the predicted score
+            return jsonify({'predicted_risk_score': predicted_risk_score}), 200
+        else:
+            return jsonify({'error': 'Missing required features'}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
