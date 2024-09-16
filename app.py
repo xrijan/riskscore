df52363699cd7dcd2fbc375a953d371482c5c554
Rijan Rayamajhi <rayarijan5051@gmail.com>
2024-09-17 01:02:12 +0530

    datatype removed

--- app.py
+++ app.py
@@ -39,26 +39,6 @@ def predict():
         # Convert data to DataFrame
         df = pd.DataFrame([data])
 
-        # Ensure correct dtypes to minimize memory usage
-        df = df.astype({
-            'cost_t': 'float32',
-            'age': 'float32',
-            'dem_female': 'int8',
-            'race': 'int8',
-            'biomarkers': 'float32',
-            'comorbidity': 'int8',
-            'lasix_dose_count_tm1': 'float32',
-            'cre_tests_tm1': 'float32',
-            'crp_tests_tm1': 'float32',
-            'esr_tests_tm1': 'float32',
-            'ghba1c_tests_tm1': 'float32',
-            'hct_tests_tm1': 'float32',
-            'ldl_tests_tm1': 'float32',
-            'nt_bnp_tests_tm1': 'float32',
-            'sodium_tests_tm1': 'float32',
-            'trig_tests_tm1': 'float32'
-        })
-
         # Check if all required features are present
         if all(feature in df.columns for feature in features):
             X = df[features]
