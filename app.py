cb10a05a624c5a34fc2c8f0954f3d7f3e883ba5b
Rijan Rayamajhi <rayarijan5051@gmail.com>
2024-09-17 14:27:49 +0530

    optimization

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import numpy as np
 app = Flask(__name__)
 Compress(app)
 
-
 # Define the features used for prediction
 features = ['cost_t', 'age', 'dem_female', 'race', 'biomarkers', 'comorbidity',
             'lasix_dose_count_tm1', 'cre_tests_tm1', 'crp_tests_tm1', 'esr_tests_tm1',
@@ -19,30 +18,25 @@ features = ['cost_t', 'age', 'dem_female', 'race', 'biomarkers', 'comorbidity',
 def load_scaler_and_model():
     with gzip.open('standard_scaler.pkl.gz', 'rb') as scaler_file_in:
         scaler = pickle.load(scaler_file_in)
-
     with gzip.open('random_forest_risk_score_model_compressed.pkl.gz', 'rb') as model_file_in:
         model = pickle.load(model_file_in)
-
     return scaler, model
+
+scaler, model = load_scaler_and_model()
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# API route to predict risk score
-@app.route('/predict', methods=['POST'])
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Load the scaler and model when needed
-        scaler, model = load_scaler_and_model()
-
         # Parse input data from the request
         data = request.get_json()
-
+        
         # Convert data to DataFrame
         df = pd.DataFrame([data])
-
+        
         # Ensure correct dtypes to minimize memory usage
         df = df.astype({
             'cost_t': 'float32',
@@ -68,14 +62,11 @@ def predict():
             X = df[features]
             X_scaled = scaler.transform(X)
             predicted_risk_score = model.predict(X_scaled)[0]
-            
-            # Return the predicted score
             return jsonify({'predicted_risk_score': predicted_risk_score}), 200
         else:
             return jsonify({'error': 'Missing required features'}), 400
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, use_reloader=False)  # Turn off debug mode and auto-reloader for production
