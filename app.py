d2d1c2276f02f59f68bc68de401b2faced2ef7df
Rijan Rayamajhi <rayarijan5051@gmail.com>
2024-09-17 13:49:35 +0530

    optimization performance

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify, render_template
+from flask_compress import Compress
 import pickle
 import gzip
 import pandas as pd
 import numpy as np
 
 app = Flask(__name__)
+Compress(app)
+
 
 # Define the features used for prediction
 features = ['cost_t', 'age', 'dem_female', 'race', 'biomarkers', 'comorbidity',
@@ -12,8 +15,8 @@ features = ['cost_t', 'age', 'dem_female', 'race', 'biomarkers', 'comorbidity',
             'ghba1c_tests_tm1', 'hct_tests_tm1', 'ldl_tests_tm1', 'nt_bnp_tests_tm1',
             'sodium_tests_tm1', 'trig_tests_tm1']
 
+# Load the scaler and model once on startup
 def load_scaler_and_model():
-    # Decompress and load the saved scaler and model only when needed
     with gzip.open('standard_scaler.pkl.gz', 'rb') as scaler_file_in:
         scaler = pickle.load(scaler_file_in)
 
@@ -21,6 +24,8 @@ def load_scaler_and_model():
         model = pickle.load(model_file_in)
 
     return scaler, model
+
+scaler, model = load_scaler_and_model()
 
 @app.route('/')
 def home():
@@ -30,9 +35,6 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Load the scaler and model only when needed
-        scaler, model = load_scaler_and_model()
-        
         # Parse input data from the request
         data = request.get_json()
 
@@ -64,9 +66,6 @@ def predict():
             X = df[features]
             X_scaled = scaler.transform(X)
             predicted_risk_score = model.predict(X_scaled)[0]
-
-            # Clear variables to free memory
-            del scaler, model, df, X, X_scaled
             
             # Return the predicted score
             return jsonify({'predicted_risk_score': predicted_risk_score}), 200
